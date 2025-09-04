b5ea596a1a07bc41439f08156d46cd0c7e307099
arull <arull@DHANALAXMI>
2025-09-04 22:07:20 +0530

    Spring ioc Scope

---
+++ SpringIOC_Scope/src/main/java/com/tnsif/scope/Product.java
@@ -0,0 +1,15 @@
+package com.tnsif.scope;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component
+@Scope(value="prototype")
+public class Product {
+	private int id;
+	private String p_nmae;
+	public Product() {
+		System.out.println("Product class object has been created");
+	}
+
+}
