26701edca92bbf00ca4402d6eb7b7dcbe345a6c7
arull <arull@DHANALAXMI>
2025-09-05 12:08:25 +0530

    SpringQualifier

---
+++ springqualifier/src/main/java/com/tnsif/springq/Electronics.java
@@ -0,0 +1,11 @@
+package com.tnsif.springq;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Electronics implements ShoppingMall {
+	public void purchase() {
+		System.out.println("I'm going to purchase some widges..");
+	}
+
+}
