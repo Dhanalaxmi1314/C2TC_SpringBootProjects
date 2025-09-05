e203b1fc5ce74144af37c7540f9b8210991b08b5
arull <arull@DHANALAXMI>
2025-09-05 12:07:11 +0530

    completed

--- restfulapi1/src/main/java/com/tnsif/restapi/RestControllerExample.java
+++ restfulapi1/src/main/java/com/tnsif/restapi/RestControllerExample.java
@@ -1,14 +1,28 @@
 package com.tnsif.restapi;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class RestControllerExample {
-	@RequestMapping("/hello")
+	@RequestMapping("/hello Coaders")
 	public String show()
 	{
 		return "Welcome";
 	}
+	@GetMapping("/hello")
+	public String greetings(@RequestParam String name)
+	{
+		return "Welcome "+name;
+	}
+	@GetMapping("/hello/{name}")
+	public String message(@PathVariable String name)
+	{
+		return "Welcome "+name;
+	}
 
+	
 }
